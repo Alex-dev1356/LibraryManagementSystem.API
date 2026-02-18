9c645925eb3f5125803b8ceafc3c11d051c3108c
Alex-dev1356 <asoliven26@yahoo.com>
2026-02-18 13:49:54 +0800

    Seeting Up the fields for Book Model

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,23 @@
-﻿namespace LibraryManagementSystem.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.API.Models
 {
     public class Book
     {
+        public int BookId { get; set; }
+
+        [Required, StringLength(200)]
+        public string Title { get; set; }
+
+        [Required, StringLength(100)]
+        public string Author { get; set; }
+
+        [Required, StringLength(13)]
+        public string ISBN { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public ICollection<BookCategory> BookCategories { get; set; }
+        public ICollection<Loan> Loans { get; set; }
+
     }
 }
